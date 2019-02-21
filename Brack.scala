a381811145f5c67e03fce9f4defbb451661f0d69
DanBichir <46904874+DanBichir@users.noreply.github.com>
2019-02-21 17:58:54 +0000

    Update Brack.scala

--- Brack.scala
+++ Brack.scala
@@ -160,11 +160,12 @@ object Brack{
 		var i = 0; var k = 2;
 		var left = 0; var right = 0
 		var pos = 0;
+		/*Task 4*/
 		while(i<n-1){
-			poss(i)(i+1)(op(w(i))(w(j)))=true
-			poss(i)(i+1)((op(w(i))(w(j))+1)%3)=false
-			poss(i)(i+1)((op(w(i))(w(j))+2)%3)=false
-`			i += 1
+			poss(i)(i+1)(op(w(i))(w(i+1)))=true
+			poss(i)(i+1)((op(w(i))(w(i+1))+1)%3)=false
+			poss(i)(i+1)((op(w(i))(w(i+1))+2)%3)=false
+			i += 1
 		}
 		while(k<n){
 			i=0
@@ -174,13 +175,13 @@ object Brack{
 				poss(i)(i+k)(1) = false
 				poss(i)(i+k)(2) = false
 				pos = i+1
-				while(pos < j){
+				while(pos < i+k){
 					left = 0
 					while(left < 3){
 						right = 0
 						while(right<3){
 							if(poss(i)(pos)(left) == true && poss(pos)(i+k)(right) == true)
-								poss(i)(j)(op(left)(right)) = true
+								poss(i)(i+k)(op(left)(right)) = true
 							right+=1
 						}
 						left+=1
@@ -190,14 +191,15 @@ object Brack{
 				}
 				i+=1
 			}
-			k=+1
+			k+=1
 		}
+		/*Task 5*/
 		var ways = Array.ofDim[Int](MAXWORD, MAXWORD, 3)
 		while(i<n-1){
-			ways(i)(i+1)(op(w(i))(w(j)))=1
-			ways(i)(i+1)((op(w(i))(w(j))+1)%3)=0
-			ways(i)(i+1)((op(w(i))(w(j))+2)%3)=0
-`			i += 1
+			ways(i)(i+1)(op(w(i))(w(i+1)))=1
+			ways(i)(i+1)((op(w(i))(w(i+1))+1)%3)=0
+			ways(i)(i+1)((op(w(i))(w(i+1))+2)%3)=0
+			i += 1
 		}
 		k=2;
 		while(k<n){
@@ -208,12 +210,12 @@ object Brack{
 				ways(i)(i+k)(1) = 0
 				ways(i)(i+k)(2) = 0
 				pos = i+1
-				while(pos < j){
+				while(pos < i+k){
 					left = 0
 					while(left < 3){
 						right = 0
 						while(right<3){
-								ways(i)(j)(op(left)(right)) = ways(i)(j)(op(left)(right)) + ways(i)(pos)(left) * ways(pos)(i+k)(right)
+								ways(i)(i+k)(op(left)(right)) = ways(i)(i+k)(op(left)(right)) + ways(i)(pos)(left) * ways(pos)(i+k)(right)
 							right+=1
 						}
 						left+=1
@@ -223,204 +225,9 @@ object Brack{
 				}
 				i+=1
 			}
-			k=+1
+			k+=1
 		}
 		
-	}
-
-	//Task 6
-	//TODO Runtime analysis of dynamic programming version with tests
-  
-
-/** The main method just selects which piece of functionality to run */
-  def main(args: Array[String]) = {
-
-    // string to print if error occurs
-    val errString = 
-      "Usage: scala Brack -PossibleRec [file]\n"+
-      "     | scala Brack -NumberRec [file]\n"+
-      "     | scala Brack -Tabulate [file]\n"
-		
-		if (args.length > 2){
-			println(errString)
-			sys.exit
-		}
-
-    //Get the plaintext, either from the file whose name appears in position
-    //pos, or from standard input
-    def getPlain(pos: Int) = 
-      if(args.length==pos+1) readFile(args(pos)) else StdIn.readLine.toArray
-
-    // Check there are at least n arguments
-    def checkNumArgs(n: Int) = if(args.length<n){println(errString); sys.exit}
-
-    // Parse the arguments, and call the appropriate function
-    checkNumArgs(1)
-		val plain = getPlain(1)
-    val command = args(0)
-
-		//Making sure the letters are of the right type
-		val len = plain.length
-		var plainInt = new Array[Int](len)
-		if (len > MAXWORD){
-			println("Word Too Long! Change MAXWORD")
-			sys.exit;
-		} else {
-    	for (i <- 0 until len){
-				plainInt(i) = LetterToInt(plain(i))
-			}
-		}
-		
-		//Executing appropriate command
-    if(command=="-PossibleRec"){
-		println("Bracketing values for "+ plain.mkString(""))
-		for(i<-0 to 2){
-			if(PossibleRec(plainInt, 0, len, i)){
-				println(('A'.toInt + i).toChar + " is Possible");
-			}
-			else{
-				println(('A'.toInt + i).toChar + " is not Possible");
-			}
-		}
-    }
-    else if(command=="-NumberRec"){
-		var z: Int = 0
-		println("Bracketing values for "+ plain.mkString(""))
-		for(i<-0 to 2){
-			z = NumberRec(plainInt, 0, len, i)
-			if(z == 1){
-				printf(('A'.toInt + i).toChar+ " can be achieved in %d way\n", z)
-			}
-			else{
-				printf(('A'.toInt + i).toChar+ " can be achieved in %d ways\n", z)
-			}
-		}
-    }
-
-    else if(command=="-Tabulate"){
-		Tabulate(plainInt,len)
-		println("Bracketing values for "+ plain.mkString(""))
-		for(v<-0 to 2){
-		var z: Int = ways(0)(len)(v)
-			if(z==0){
-			println(('A'.toInt + v).toChar+ " cannot be achieved")
-			}
-			else if(z==1){
-				printf(('A'.toInt + v).toChar+ " can be achieved %d way\n", z)
-				printf("For example:")
-				print_tree(exp(0)(len)(v))
-				printf("\n")
-			}
-			else if (z > 1){
-				printf(('A'.toInt + v).toChar+ " can be achieved %d ways\n", z)
-				printf("For example:")
-				print_tree(exp(0)(len)(v))
-				printf("\n")
-			}
-		}
-    }      
-    else println(errString)
-  }
-}	
-	//TASK 2
-	//NumberRec which checks the ways you get a result recursively
-	//Computes number of ways w(i,j) can be bracketed to get z
-	
-	def NumberRec(w: Array[Int], i: Int, j: Int, z:Int): Int = {
-	//TODO
-	if(j==i+1 && w(i) == z)	return 1
-	else	if(j==i+1) 	return 0
-	
-	var left = 0 ; var right = 0;
-	var pos = i+1
- 	var max = 0
-	while(pos < j){
-		left = 0
-		while(left < 3){
-			right = 0
-			while(right<3){
-				if(op(left)(right) == z)
-					max=max+NumberRec(w,i,pos,left)*NumberRec(w,pos,j,right)
-						
-				right+=1
-			}
-			left+=1
-		}
-		pos+=1
-	}
-	return max
-	} 
-
-	
-	//TASK 3
-	//TODO Runtime analysis of recursive solution along with tests
-	
-	/*tw40[~/daa]$ time scala Brack -NumberRec testcase
-
-	Bracketing values for ABBA
-	A can be achieved in 2 ways
-	B can be achieved in 1 way
-	C can be achieved in 2 ways
-
-	real	0m0.577s
-	user	0m0.772s
-	sys	0m0.050s
-
-	Bracketing values for ABC
-	A can be achieved in 1 way
-	B can be achieved in 1 way
-	C can be achieved in 0 ways
-
-	real	0m0.572s
-	user	0m0.757s
-	sys	0m0.060s
-
-	Bracketing values for ABCBACCB
-	A can be achieved in 136 ways
-	B can be achieved in 224 ways
-	C can be achieved in 69 ways
-
-	real	0m0.604s
-	user	0m0.799s
-	sys	0m0.061s
-
-	Bracketing values for ABCBACCBBAC
-	A can be achieved in 7904 ways
-	B can be achieved in 4688 ways
-	C can be achieved in 4204 ways
-
-	real	0m5.403s
-	user	0m5.593s
-	sys	0m0.054s
-
-
-
-
-	
-	*/
-	//You may find the following class useful for Task 7
-	// Binary tree class
-	abstract class BinaryTree
-	case class Node (left : BinaryTree, right : BinaryTree) extends BinaryTree
-	case class Leaf (value : Char) extends BinaryTree
-
-	//Printing for a binary tree
-	def print_tree(t : BinaryTree) {
-	//TODO(optional)
-	}
-
-	//These arrays should hold the relevant data for dynamic programming
-	var poss = Array.ofDim[Boolean](MAXWORD, MAXWORD, 3)
-	var ways = Array.ofDim[Int](MAXWORD, MAXWORD, 3)
-	var exp = Array.ofDim[BinaryTree](MAXWORD, MAXWORD, 3)
-
-
-	//Task 4, 5, and 7(optional)
-	//TODO Fill out arrays with dynamic programming solution
-	
-	
-	def Tabulate(w: Array[Int], n: Int): Unit = {
-	//TODO
 	}
 
 	//Task 6
